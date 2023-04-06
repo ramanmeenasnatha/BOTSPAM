9ab40468bc28a36bc7a8cb45b309b91787c64f79
ramanmeenasnatha <125035770+ramanmeenasnatha@users.noreply.github.com>
2023-04-06 11:21:04 +0530

    Update __init__.py

--- LegendGirl/__init__.py
+++ LegendGirl/__init__.py
@@ -4,7 +4,7 @@ from .Config import *
 from .core import *
 
 version = "v1"
-group_username = "@LegendBotSpam"
+group_username = "@MrDevid_KaHelpGroup"
 start_time = time.time()
 # Sudo Users
 sudos = []
